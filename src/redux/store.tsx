486aafe62c01574e32a57ccfaf43207daab858f8
Voevodskiy Egor <voevodskiy.ea@gmail.com>
2021-04-06 20:38:37 +0300

    new Structure

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,5 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './rootReducer';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
+import rootReducer from './rootReducer';
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, thunk)));
